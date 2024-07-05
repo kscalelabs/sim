815a9013d39fdf63f136d23649420f39f481bb9b
is2ac <wlight@emory.edu>
2024-07-05 11:52:27 -0700

    removing torques values the model doesn't fly away

--- sim/humanoid_gym/envs/stompy_config.py
+++ sim/humanoid_gym/envs/stompy_config.py
@@ -74,7 +74,7 @@ class StompyCfg(LeggedRobotCfg):
         restitution = 0.0
 
     class noise:
-        add_noise = True
+        add_noise = False
         noise_level = 0.6  # scales other values
 
         class noise_scales:
@@ -86,7 +86,7 @@ class StompyCfg(LeggedRobotCfg):
             height_measurements = 0.1
 
     class init_state(LeggedRobotCfg.init_state):
-        pos = [0.0, 0.0, 1.05]
+        pos = [0.0, 0.0, 1.15] # THIS
 
         default_joint_angles = {k: 0.0 for k in Stompy.all_joints()}
 
@@ -95,7 +95,7 @@ class StompyCfg(LeggedRobotCfg):
             default_joint_angles[joint] = default_positions[joint]
 
     class control(LeggedRobotCfg.control):
-        # PD Drive parameters:
+        # PD Drive parameters: THIS
         stiffness = {
             "x10": 200,
             "x8": 200,
@@ -129,11 +129,11 @@ class StompyCfg(LeggedRobotCfg):
 
         class physx(LeggedRobotCfg.sim.physx):
             num_threads = 12
-            solver_type = 1  # 0: pgs, 1: tgs
-            num_position_iterations = 4
-            num_velocity_iterations = 0
-            contact_offset = 0.01  # [m]
-            rest_offset = -0.02  # [m]
+            solver_type = 1  # 0: pgs, 1: tgs THIS
+            num_position_iterations = 4  # THIS
+            num_velocity_iterations = 1 # THIS
+            contact_offset = 0.01  # [m] THIS
+            rest_offset = -0.02  # [m] THIS
             bounce_threshold_velocity = 0.1  # [m/s]
             max_depenetration_velocity = 1.0
             max_gpu_contact_pairs = 2**23  # 2**24 -> needed for 8000 envs and more
@@ -142,12 +142,12 @@ class StompyCfg(LeggedRobotCfg):
             contact_collection = 2
 
     class domain_rand:
-        randomize_friction = True
+        randomize_friction = False
         friction_range = [0.1, 2.0]
 
-        randomize_base_mass = True
+        randomize_base_mass = False
         added_mass_range = [-1.0, 1.0]
-        push_robots = True
+        push_robots = False
         push_interval_s = 4
         max_push_vel_xy = 0.2
         max_push_ang_vel = 0.4
