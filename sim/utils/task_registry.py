f887d1f03555cbafc4c2e8f28a01a461eadf97bf
budzianowski <budzianowski@gmail.com>
2024-08-23 15:00:23 -0700

    saving the untyped-def

--- sim/utils/task_registry.py
+++ sim/utils/task_registry.py
@@ -28,7 +28,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # Copyright (c) 2024 Beijing RobotEra TECHNOLOGY CO.,LTD. All rights reserved.
-
+# mypy: disable-error-code="no-untyped-def"
 
 import os
 from datetime import datetime
