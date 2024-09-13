b1fb77b9d277d909dd491bfe86a4ed30e4313406
kewang <superwk1017@gmail.com>
2024-09-13 15:50:30 +0100

    improve the code by defining the urdf path in config instead of hard-coded in env.py

--- sim/envs/humanoids/h1_config.py
+++ sim/envs/humanoids/h1_config.py
@@ -36,9 +36,8 @@ class H1Cfg(LeggedRobotCfg):
         torque_limit = 0.85
 
     class asset(LeggedRobotCfg.asset):
-        file = str(robot_urdf_path())
-
-        name = "h1"
+        name = "h1_2"
+        file = str(robot_urdf_path(name))
 
         foot_name = "ankle_roll"
         knee_name = "knee_link"
--- sim/envs/humanoids/stompymini_config.py
+++ sim/envs/humanoids/stompymini_config.py
@@ -33,9 +33,9 @@ class MiniCfg(LeggedRobotCfg):
         torque_limit = 0.85
 
     class asset(LeggedRobotCfg.asset):
-        file = str(robot_urdf_path())
+        name = "stompymini"
+        file = str(robot_urdf_path(name))
 
-        name = "stompy"
 
         foot_name = ["RS_01_Stator", "RS_01_Stator_2"]
         knee_name = ["RS_04_Rotor_7", "RS_04_Rotor_8"]
