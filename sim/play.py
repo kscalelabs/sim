a632225e32438ed921221f03a336be7b1ae99107
Henri <henri123lemoine@gmail.com>
2024-09-30 20:09:21 -0400

    nit

--- sim/play.py
+++ sim/play.py
@@ -175,8 +175,7 @@ def play(args: argparse.Namespace) -> None:
             env.commands[:, 1] = 0.0
             env.commands[:, 2] = 0.0
             env.commands[:, 3] = 0.0
-        obs, critic_obs, rews, dones, infos = env.step(actions.detach())
-        print(f"IMU: {obs[0, (3 * env.num_actions + 5) + 3 : (3 * env.num_actions + 5) + 2 * 3]}")
+        obs, _, _, _, infos = env.step(actions.detach())
 
         if RENDER:
             env.gym.fetch_results(env.sim, True)
