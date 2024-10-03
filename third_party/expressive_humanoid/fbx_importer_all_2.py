70cb0929cf6e0be53b98750fe0676c5dd5eea8a4
budzianowski <budzianowski@gmail.com>
2024-10-03 15:00:52 -0700

    fix linting

--- third_party/expressive_humanoid/fbx_importer_all_2.py
+++ third_party/expressive_humanoid/fbx_importer_all_2.py
@@ -1,64 +1,43 @@
+"""This script converts CMU Motion Capture data from FBX format to NPY format. """
 import sys
-sys.path.append("/home/user/fbx_setup/fbx_python_bindings/build/Distrib/site-packages/fbx/")
 import os
-import multiprocessing
 from tqdm import tqdm
 from poselib.skeleton.skeleton3d import SkeletonMotion
+import multiprocessing
+
+sys.path.append("/home/user/fbx_setup/fbx_python_bindings/build/Distrib/site-packages/fbx/")
 
 def process_file(i, fbx_file, all_fbx_path):
     if fbx_file.endswith(".fbx"):
         print(i, fbx_file)
         motion = SkeletonMotion.from_fbx(
-            fbx_file_path=all_fbx_path + fbx_file,
+            fbx_file_path=os.path.join(all_fbx_path, fbx_file),
             root_joint="Hips",
             fps=60
         )
         motion.to_file(f"data/npy/{fbx_file[:-4]}.npy")
-    # try:
-    #     if fbx_file.endswith(".fbx"):
-    #         print(i, fbx_file)
-    #         motion = SkeletonMotion.from_fbx(
-    #             fbx_file_path=all_fbx_path + fbx_file,
-    #             root_joint="Hips",
-    #             fps=60
-    #         )
-    #         motion.to_file(f"data/npy/{fbx_file[:-4]}.npy")
-    # except:
-    #     print(f"Error in {fbx_file}")
-    #     pass
 
 def main():
     all_fbx_path = "data/cmu_fbx_all/"
-    all_fbx_files = os.listdir(all_fbx_path)
-    all_fbx_files.sort()
+    all_fbx_files = sorted(os.listdir(all_fbx_path))
 
     all_fbx_filtered = []
     for fbx in all_fbx_files:
         npy = fbx.split(".")[0] + ".npy"
-        target_motion_file = os.path.join(all_fbx_path, "../npy/" + npy)
+        target_motion_file = os.path.join(all_fbx_path, "../npy/", npy)
         if os.path.exists(target_motion_file):
             print("Already exists, skip: ", fbx)
             continue
         all_fbx_filtered.append(fbx)
-    all_fbx_filtered.sort()
-    print(len(all_fbx_filtered))
-    # breakpoint()
-    for fbx_file in tqdm(all_fbx_files):
-        if fbx_file.endswith(".fbx"):
-            print(fbx_file)
-            motion = SkeletonMotion.from_fbx(
-                fbx_file_path=all_fbx_path + fbx_file,
-                root_joint="Hips",
-                fps=60
-            )
-            motion.to_file(f"data/npy/{fbx_file[:-4]}.npy")
-    # Number of processes
-    # n_workers = multiprocessing.cpu_count()
 
-    # # Create a pool of worker processes
-    # with multiprocessing.Pool(n_workers) as pool:
-    #     # Using starmap to pass multiple arguments to the process_file function
-    #     list(tqdm(pool.starmap(process_file, [(i, fbx_file, all_fbx_path) for i, fbx_file in enumerate(all_fbx_filtered)]), total=len(all_fbx_files)))
+    print(len(all_fbx_filtered))
+
+    for fbx_file in tqdm(all_fbx_filtered):
+        process_file(None, fbx_file, all_fbx_path)
+
+    n_workers = multiprocessing.cpu_count()
+    with multiprocessing.Pool(n_workers) as pool:
+        list(tqdm(pool.starmap(process_file, [(i, fbx_file, all_fbx_path) for i, fbx_file in enumerate(all_fbx_filtered)]), total=len(all_fbx_filtered)))
 
 if __name__ == "__main__":
     main()
--- third_party/expressive_humanoid/parse_cmu_mocap_all_2.py
+++ third_party/expressive_humanoid/parse_cmu_mocap_all_2.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from ruamel.yaml import YAML
-import numpy as np
 
 path = "./data/configs/"
+
 def find_entries(word, column):
     subset = column[column.str.contains(word, na=False, case=False)]
     num_entries = len(subset)
@@ -15,26 +14,20 @@ def find_entries(word, column):
 
 def save_yaml(motion_ids, motion_descriptions, name):
     yaml = YAML()
-    yaml.preserve_quotes = True  # Preserve quotes if necessary
+    yaml.preserve_quotes = True
     motions_data = {"root": "../retarget_npy/"}
     for i, motion_id in enumerate(motion_ids):
-        descrp = motion_descriptions[i]
-        descrp = descrp.replace("/", ", ").replace("\\", ", ")
+        descrp = motion_descriptions[i].replace("/", ", ").replace("\\", ", ")
         motions_data[motion_id] = {
-            "trim_beg": -1,  # Assuming these values are constant, modify as needed
+            "trim_beg": -1,
             "trim_end": -1,
             "weight": 1.0,
             "description": f"{descrp}",
-            "difficulty": 4  # Modify as needed
+            "difficulty": 4
         }
-    # Constructing the final dictionary structure
-    final_structure = {
-        "motions": motions_data
-    }
-    # Writing to a YAML file
+    final_structure = {"motions": motions_data}
     with open(path + f'motions_autogen_debug_{name}.yaml', 'w') as file:
         yaml.dump(final_structure, file)
-
     print('YAML file created with the specified structure.')
 
 file_path = path + "cmu-mocap-index-spreadsheet.xls"
@@ -42,19 +35,15 @@ data = pd.read_excel(file_path)
 description_column = data.iloc[:, 1]
 motion_id_column = data.iloc[:, 0]
 
-forbbiden_words = ["ladder", "suitcase", "uneven", "terrain", "stair", "stairway", "stairwell", "clean", "box", "climb", "backflip", "handstand", "sit", "hang"]
-# target_words = ["walk", "run", "jump", "navigate", "basketball", "dance", "punch", "fight", "push", "pull", "throw", "catch", "crawl", "wave", "high five", "hug", "drink", "wash", "signal", "balance", "strech", "leg", "bend", "squat", "traffic", "high-five", "low-five"]
+forbidden_words = ["ladder", "suitcase", "uneven", "terrain", "stair", "stairway", "stairwell", "clean", "box", "climb", "backflip", "handstand", "sit", "hang"]
 target_words = ["walk", "navigate", "basketball", "dance", "punch", "fight", "push", "pull", "throw", "catch", "crawl", "wave", "high five", "hug", "drink", "wash", "signal", "balance", "strech", "leg", "bend", "squat", "traffic", "high-five", "low-five"]
-# target_words = ["walk", "navigate"]
-target_results = []
-for word in target_words:
-    target_results.append(find_entries(word, description_column))
+
+target_results = [find_entries(word, description_column) for word in target_words]
 
 print("\n Searching for forbidden words:")
 fbd_indices = []
-for word in forbbiden_words:
+for word in forbidden_words:
     fbd_indices.extend(find_entries(word, description_column)[2])
-print(f"Found {len(fbd_indices)} forbidden entries.")
 fbd_indices = list(set(fbd_indices))
 print(f"Found {len(fbd_indices)} unique forbidden entries.")
 
@@ -76,41 +65,3 @@ for i, result in enumerate(target_results):
 indices_all_unique = list(set(indices_all))
 motion_ids_all_unique = motion_id_column[indices_all_unique]
 save_yaml(motion_ids_all_unique, description_column[indices_all_unique].tolist(), "all_no_run_jump")
-
-
-# exit()
-
-# text_all = description_column[indices_all_unique].tolist()
-# from sentence_transformers import SentenceTransformer
-
-# model = SentenceTransformer('sentence-transformers/paraphrase-MiniLM-L3-v2')
-# embeddings = model.encode(text_all)
-# print(embeddings.shape)
-
-# from sklearn.cluster import KMeans
-# k = 8  # Example number of clusters
-# kmeans = KMeans(n_clusters=k, random_state=0).fit(embeddings)
-
-# # Get cluster labels for each point in your dataset
-# labels = kmeans.labels_
-# np.save("labels.npy", labels)
-
-# import matplotlib.pyplot as plt
-# from sklearn.decomposition import PCA
-# # Reduce dimensions (e.g., to 2D)
-# pca = PCA(n_components=2)
-# reduced_embeddings = pca.fit_transform(embeddings)
-
-# # Plot
-# plt.scatter(reduced_embeddings[:, 0], reduced_embeddings[:, 1], c=labels, cmap='viridis')
-# plt.xlabel('Component 1')
-# plt.ylabel('Component 2')
-# plt.title('2D Visualization of Embeddings Clusters')
-# # plt.legend([0, 1, 2, 3, 4])
-# plt.show()
-# # plt.savefig("cluster.png")
-
-# # get all text for label 0
-# for i_cluster in range(k):
-#     indices_label_0 = [i for i, label in enumerate(labels) if label == i_cluster]
-#     print([text_all[i] for i in indices_label_0], "\n")
