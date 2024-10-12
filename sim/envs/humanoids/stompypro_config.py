f1ec7124670a490697e70665e124fe52f6dca197
budzianowski <budzianowski@gmail.com>
2024-10-11 19:32:35 -0700

    update files

--- sim/envs/humanoids/stompypro_config.py
+++ sim/envs/humanoids/stompypro_config.py
@@ -262,7 +262,7 @@ class StompyProCfgPPO(LeggedRobotCfgPPO):
         policy_class_name = "ActorCritic"
         algorithm_class_name = "PPO"
         num_steps_per_env = 60  # per iteration
-        max_iterations = 8001  # number of policy updates
+        max_iterations = 3001  # number of policy updates
 
         # logging
         save_interval = 100  # check for potential saves every this many iterations
