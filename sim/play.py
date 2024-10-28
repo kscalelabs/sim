4b269c022a766ec35a0bbc1d37891a22a568150a
budzianowski <budzianowski@gmail.com>
2024-10-28 16:57:16 -0700

    current setup behaves weirdly

--- sim/play.py
+++ sim/play.py
@@ -171,7 +171,7 @@ def play(args: argparse.Namespace) -> None:
             dset_actions[t] = actions.detach().numpy()
 
         if FIX_COMMAND:
-            env.commands[:, 0] = 0.5
+            env.commands[:, 0] = 0.15
             env.commands[:, 1] = 0.0
             env.commands[:, 2] = 0.0
             env.commands[:, 3] = 0.0
