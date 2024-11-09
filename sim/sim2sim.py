d38fa4a394b69c064185b64bbaf04c83139633b8
Henri <henri123lemoine@gmail.com>
2024-11-09 01:50:30 -0500

    Added sim2sim_helpers.py

--- sim/sim2sim.py
+++ sim/sim2sim.py
@@ -49,30 +49,9 @@ from scipy.spatial.transform import Rotation as R
 from tqdm import tqdm
 
 from sim.scripts.create_mjcf import load_embodiment
+from sim.sim2sim_helpers import handle_keyboard_input
 
 import torch  # isort: skip
-
-
-def handle_keyboard_input():
-    global x_vel_cmd, y_vel_cmd, yaw_vel_cmd
-
-    keys = pygame.key.get_pressed()
-
-    # Update movement commands based on arrow keys
-    if keys[pygame.K_UP]:
-        x_vel_cmd += 0.0005
-    if keys[pygame.K_DOWN]:
-        x_vel_cmd -= 0.0005
-    if keys[pygame.K_LEFT]:
-        y_vel_cmd += 0.0005
-    if keys[pygame.K_RIGHT]:
-        y_vel_cmd -= 0.0005
-
-    # Yaw control
-    if keys[pygame.K_a]:
-        yaw_vel_cmd += 0.001
-    if keys[pygame.K_z]:
-        yaw_vel_cmd -= 0.001
 
 
 class Sim2simCfg:
