cae7a6271ef86fcaf7c169aefe2cc0b996d7d5bc
alik-git <ali@kscale.dev>
2024-11-20 20:21:20 +0000

    add cmd arg for h5 output path with sensible default

--- sim/h5_logger.py
+++ sim/h5_logger.py
@@ -6,22 +6,31 @@ from typing import Dict
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime
 
 
 class HDF5Logger:
-    def __init__(self, data_name: str, num_actions: int, max_timesteps: int, num_observations: int):
+    def __init__(self, data_name: str, num_actions: int, max_timesteps: int, num_observations: int, h5_out_dir: str = "sim/resources/"):
         self.data_name = data_name
         self.num_actions = num_actions
         self.max_timesteps = max_timesteps
         self.num_observations = num_observations
         self.max_threshold = 1e3  # Adjust this threshold as needed
+        self.h5_out_dir = h5_out_dir
         self.h5_file, self.h5_dict = self._create_h5_file()
         self.current_timestep = 0
 
     def _create_h5_file(self):
         # Create a unique file ID
         idd = str(uuid.uuid4())
-        h5_file = h5py.File(f"{self.data_name}/{idd}.h5", "w")
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+        curr_h5_out_dir = f"{self.h5_out_dir}/{self.data_name}/h5_out/"
+        os.makedirs(curr_h5_out_dir, exist_ok=True)
+
+        h5_file_path = f"{curr_h5_out_dir}/{timestamp}__{idd}.h5"
+        print(f"Saving HDF5 data to {h5_file_path}")
+        h5_file = h5py.File(h5_file_path, "w")
 
         # Create datasets for logging actions and observations
         dset_actions = h5_file.create_dataset("prev_actions", (self.max_timesteps, self.num_actions), dtype=np.float32)
