9c43475b8960488d1a9b8000eee65cc93ccc8e48
alik-git <ali@kscale.dev>
2024-11-20 06:41:43 +0000

    revert commit "temp patch for download assets. we need to host these files somewhere they can be downloaded via CLI"

--- sim/scripts/download_assets.sh
+++ sim/scripts/download_assets.sh
@@ -1,35 +1,29 @@
 #!/bin/zsh
 
-# # Stompymini
-# gdown --id 1Ah92CTN63M2h4uKPLf8eXkX2H9Bg3Kji
-# unzip meshes.zip -d sim/resources/stompymini/
-# rm meshes.zip
+# Stompymini
+gdown --id 1Ah92CTN63M2h4uKPLf8eXkX2H9Bg3Kji
+unzip meshes.zip -d sim/resources/stompymini/
+rm meshes.zip
 
 # Stompypro
-echo "2. Stompypro:"
-echo "Since gdown has a permissions issue, you need to download the files manually."
-echo "   - Visit: https://drive.google.com/drive/folders/1-iIqy8j4gF6JeuMc_MjxkRe4vSZl8Ozp"
-echo "   - Download all files and place them in 'sim/resources/stompypro/'"
+gdown --folder https://drive.google.com/drive/folders/1-iIqy8j4gF6JeuMc_MjxkRe4vSZl8Ozp -O sim/resources/stompypro/
 
-# Stompypro
-# gdown --folder https://drive.google.com/drive/folders/1-iIqy8j4gF6JeuMc_MjxkRe4vSZl8Ozp -O sim/resources/stompypro/
+# Xbot
+gdown --id 1tpl95OdUhg9VL88FhKWMReY4qtLHHKoh
+tar -xzvf meshes.zip -C sim/resources/xbot/
+rm meshes.zip
 
-# # Xbot
-# gdown --id 1tpl95OdUhg9VL88FhKWMReY4qtLHHKoh
-# tar -xzvf meshes.zip -C sim/resources/xbot/
-# rm meshes.zip
+# Dora
+gdown --folder https://drive.google.com/drive/folders/1tQiMtOwGg3PGo9AygX3sj6X_HBpnW2S_ -O sim/resources/dora/
 
-# # Dora
-# gdown --folder https://drive.google.com/drive/folders/1tQiMtOwGg3PGo9AygX3sj6X_HBpnW2S_ -O sim/resources/dora/
+# G1
+gdown --folder https://drive.google.com/drive/folders/1OxYcIJpeih89NY5auRayxCXpndrRnLJx -O sim/resources/g1/
 
-# # G1
-# gdown --folder https://drive.google.com/drive/folders/1OxYcIJpeih89NY5auRayxCXpndrRnLJx -O sim/resources/g1/
+# H1_2
+gdown --id 19ih7zG6Ky8xJVJD5M1th2hmqtxaNiZyh
+tar -xzvf meshes.zip -C sim/resources/h1_2/
+rm meshes.zip
 
-# # H1_2
-# gdown --id 19ih7zG6Ky8xJVJD5M1th2hmqtxaNiZyh
-# tar -xzvf meshes.zip -C sim/resources/h1_2/
-# rm meshes.zip
-
-# # Stompymicro
-# gdown --folder https://drive.google.com/drive/folders/1C_v0FKoc6um0tUK2f1e6cWXtfvuc-xsD -O sim/resources/stompymicro/
+# Stompymicro
+gdown --folder https://drive.google.com/drive/folders/1C_v0FKoc6um0tUK2f1e6cWXtfvuc-xsD -O sim/resources/stompymicro/
 
