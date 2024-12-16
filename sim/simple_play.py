ca52534c89bc6761d67513e0e87a81746656bce5
Henri <henri123lemoine@gmail.com>
2024-12-16 00:34:42 -0500

    added modifiable fixed command and simple_play.py default sim params

--- sim/simple_play.py
+++ sim/simple_play.py
@@ -1,6 +1,4 @@
 import argparse
-from isaacgym import gymapi
-import torch
 
 from sim.env_helpers import debug_robot_state
 from sim.utils.args_parsing import parse_args_with_extras
@@ -11,21 +9,30 @@ from sim.utils.helpers import (
     parse_sim_params,
 )
 from sim.algo.ppo.on_policy_runner import OnPolicyRunner
+import torch # isort: skip
 
 
-def play(args: argparse.Namespace) -> None:
-    LOAD_MODEL_PATH = "examples/experiments/standing/robustv1/policy_1.pt"
+def simple_play(args: argparse.Namespace) -> None:
     DEVICE = "cpu"
-
+    RAND = True
     # Setup environment and training configs
-    args.load_model = LOAD_MODEL_PATH
     env_cfg, train_cfg = StompyMicroCfg(), StompyMicroCfgPPO()
     env_cfg.env.num_envs = 1
+    env_cfg.domain_rand.randomize_friction = RAND
+    env_cfg.domain_rand.randomize_base_mass = RAND
+    env_cfg.domain_rand.push_robots = RAND
+    if not RAND:
+        env_cfg.domain_rand.action_noise = 0
+        env_cfg.domain_rand.action_delay = 0
+        env_cfg.domain_rand.push_interval_s = 0
+        env_cfg.domain_rand.max_push_vel_xy = 0
+    else:
+        env_cfg.domain_rand.push_interval_s = 0.1 #1.7
+        env_cfg.domain_rand.max_push_vel_xy = 0.85
     env_cfg.sim.physx.max_gpu_contact_pairs = 2**10
-    train_cfg.runner.load_run = -8
+    train_cfg.runner.load_run = -10
     train_cfg.seed = 0
 
-    # Create and setup environment
     sim_params = {"sim": class_to_dict(env_cfg.sim)}
     sim_params = parse_sim_params(args, sim_params)
     env = StompyMicroEnv(
@@ -35,6 +42,8 @@ def play(args: argparse.Namespace) -> None:
         sim_device=args.sim_device,
         headless=False,
     )
+    command = torch.Tensor([0.0, 0.0, 0.0, 1.0]).to(DEVICE)
+    env.commands[:] = command
 
     # Setup and load policy
     all_cfg = {**class_to_dict(train_cfg), **class_to_dict(env_cfg)}
@@ -50,7 +59,7 @@ def play(args: argparse.Namespace) -> None:
     steps = int(env_cfg.env.episode_length_s / (env_cfg.sim.dt * env_cfg.control.decimation))
     for t in range(steps):
         actions = policy(obs.detach())
-        env.commands[:] = torch.zeros(4)
+        env.commands[:] = command
         obs, _, _, _, _ = env.step(actions.detach())
         if t % 17 == 0:
             debug_robot_state("Isaac", obs[0], actions[0])
@@ -59,4 +68,4 @@ def play(args: argparse.Namespace) -> None:
 if __name__ == "__main__":
     args = parse_args_with_extras(lambda x: x)
     print("Arguments:", vars(args))
-    play(args)
+    simple_play(args)
