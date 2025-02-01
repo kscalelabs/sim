08b1a54e5974d38ed95914cdc061337c724c1dc1
Wesley Maa <wesley.maa@gmail.com>
2025-02-01 15:19:50 -0800

    updated urdf

--- sim/envs/humanoids/gpr_config.py
+++ sim/envs/humanoids/gpr_config.py
@@ -17,8 +17,8 @@ class GprCfg(LeggedRobotCfg):
 
     class env(LeggedRobotCfg.env):
         # change the observation dim
-        frame_stack = 15
-        c_frame_stack = 3
+        frame_stack = 15 # Actor
+        c_frame_stack = 3 # Critic
         num_single_obs = 8 + NUM_JOINTS * 3 + 3 # Add ang vel
         num_observations = int(frame_stack * num_single_obs)
         single_num_privileged_obs = 25 + NUM_JOINTS * 4
