95986f114e76e736e6f9dc3d51efd1e93422fe4a
Wesley Maa <wesley.maa@gmail.com>
2025-02-19 13:03:21 -0800

    increase height

--- sim/envs/humanoids/gpr_latency_config.py
+++ sim/envs/humanoids/gpr_latency_config.py
@@ -49,7 +49,7 @@ class GprLatencyCfg(LeggedRobotCfg):
         knee_name = ["leg3_shell2", "leg3_shell22"]
         imu_name = "imu_link"
 
-        termination_height = 0.3
+        termination_height = 0.4 # 0.3 
         default_feet_height = 0.0
 
         terminate_after_contacts_on = ["shoulder", "shoulder_2", "arm1_top", "arm1_top_2", "arm2_shell", "arm2_shell_2",
